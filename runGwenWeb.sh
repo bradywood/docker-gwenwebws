c813739af543d4b947f87eeaad709b58dfe5cdf7
bradywood <bradyrwood@gmail.com>
2015-11-05 22:34:04 +1100

    WIP:

--- runGwenWeb.sh
+++ runGwenWeb.sh
@@ -2,10 +2,6 @@
 #
 # This bash script is to allow a user to run gwen from anywhere passing in
 # both a feature directory and report directory.
-# It will attempt to run the features against the running docker-compose selenium
-# grid.  It is making an assumption that the management of the docker-compose  
-# environment up to the user to start and scale appropriately.
-
 # Args: feature directory
 #       report directory
 
@@ -15,6 +11,7 @@ BROWSER_TYPE=${BROWSER_TYPE:-chrome}
 BROWSER_COUNT=${BROWSER_COUNT:-3}
 DEBUG=${DEBUG:-false}
 DOCKER_GWENWEB_NAME=dockergwenweb
+BROWSER_VNCPORT=
 
 PID=$$
 
@@ -24,6 +21,7 @@ then
   GWEN_IMAGE=gwen/gwenweb
 
   export FEATURE_DIRECTORY=$1
+  export META_DIRECTORY=$1
   export REPORTS_DIRECTORY=$2
 
   mkdir -p $REPORTS_DIRECTORY;
@@ -38,6 +36,7 @@ then
   if [ "$DEBUG" == true ]
   then
     DEBUG="-debug"
+    BROWSER_VNCPORT="-p 5900"
   else
     DEBUG=""
   fi
@@ -46,24 +45,28 @@ then
   for INSTANCE in $(eval echo "{0..$((BROWSER_COUNT - 1))}")
   do
     echo "starting ${BROWSER_TYPE}{DEBUG} as instance: $INSTANCE"
+    echo "need to config this if there is a proxy"
     
-    ${DOCKER_CMD} run -d -P --name ${DOCKER_GWENWEB_NAME}_${BROWSER_TYPE}_${INSTANCE} --link ${DOCKER_GWENWEB_NAME}_hub_1:hub selenium/node-${BROWSER_TYPE}${DEBUG}
+    ${DOCKER_CMD} run -d -P \
+      --name ${DOCKER_GWENWEB_NAME}_${BROWSER_TYPE}_${INSTANCE} \
+      --env="JAVA_OPTS=-trustAllSSLCertificates" \
+      --env=TZ=Australia/Sydney ${BROWSER_VNCPORT} \
+      --env=no_proxy=localhost,127.0.0.1,172.17.*,hub \
+      --link ${DOCKER_GWENWEB_NAME}_hub_1:hub \
+      selenium/node-${BROWSER_TYPE}${DEBUG}
   done
 
   #Should parse env variables.
   #allows pushing the meta files in, then is able to handle connections.
-  ${DOCKER_CMD} run --name ${DOCKER_GWENWEB_NAME}_instance_${PID} -u ${HOST_USER_ID}:${HOST_GROUP_ID} -v `pwd`:/tmp -v $FEATURE_DIRECTORY:/features -v $ABSOLUTE_REPORTS_PATH:/reports --link ${DOCKER_GWENWEB_NAME}_hub_1:hub -p 8080:8080  --workdir='/opt/gwen-web' gwen/gwenwebws bash -c "/opt/websocketd --port=8080 --devconsole gwen-web-1.0.0-SNAPSHOT/bin/gwen-web -p /opt/gwen-web/gwen.properties -m /features/*.meta"
 
-  # ${DOCKER} run -d --name ${BROWSER_TYPE}_ernie_wb_${BUILD_NUMBER}_${BROWSER_INDEX} \
-  #    -e "JAVA_OPTS=-trustAllSSLCertificates" ${PORT_BROWSERVNC} \
-  #    --link hub_ernie_wb_${BUILD_NUMBER}:hub \
-  #    --link web_ernie_wb_${BUILD_NUMBER}:web \
-  #    --link webstub_ernie_wb_${BUILD_NUMBER}:webstub \
-  #    --env=no_proxy=localhost,127.0.0.1,169.254.169.254,iod-artifactory.account,172.17.*,hub,web,webstub \
-  #    -v /usr/share/zoneinfo/Australia/Sydney:/etc/localtime \
-  #   -e=TZ=Australia/Sydney \
-  #    selenium/node-${BROWSER_TYPE}${DEBUG}
-  #done
+  #If batch mode, run features, meta and produce a report.
+  #${DOCKER_CMD} run --name ${DOCKER_GWENWEB_NAME}_instance_${PID} -u ${HOST_USER_ID}:${HOST_GROUP_ID} -v `pwd`:/tmp -v $FEATURE_DIRECTORY:/features -v $ABSOLUTE_REPORTS_PATH:/reports --link ${DOCKER_GWENWEB_NAME}_hub_1:hub -p 8080:8080  --workdir='/opt/gwen-web' gwen/gwenwebws bash -c "/opt/websocketd --port=8080 --devconsole gwen-web-1.0.0-SNAPSHOT/bin/gwen-web -p /opt/gwen-web/gwen.properties -m /features/*.meta"
+
+  #if interactive mode, run meta
+  #${DOCKER_CMD} run --name ${DOCKER_GWENWEB_NAME}_instance_${PID} -u ${HOST_USER_ID}:${HOST_GROUP_ID} -v `pwd`:/tmp -v $META_DIRECTORY:/meta -v $ABSOLUTE_REPORTS_PATH:/reports --link ${DOCKER_GWENWEB_NAME}_hub_1:hub -p 8080:8080  --workdir='/opt/gwen-web' gwen/gwenwebws bash -c "/opt/websocketd --port=8080 --devconsole gwen-web-1.0.0-SNAPSHOT/bin/gwen-web -p /opt/gwen-web/gwen.properties -m /meta/*.meta"
+  ${DOCKER_CMD} run --name ${DOCKER_GWENWEB_NAME}_instance_${PID} -u ${HOST_USER_ID}:${HOST_GROUP_ID} -v `pwd`:/tmp -v `pwd`/gwen.properties:/opt/gwen-web/gwen.properties -v $META_DIRECTORY:/features -v $ABSOLUTE_REPORTS_PATH:/reports --link grid:hub -p 8080:8080  --workdir='/opt/gwen-web' gwen/gwenwebws bash -c "/opt/websocketd --port=8080 --devconsole gwen-web-1.0.0-SNAPSHOT/bin/gwen-web -p /opt/gwen-web/gwen.properties -m /features/*.meta"
+  #needs work
+  #${DOCKER_CMD} run -it --rm --name ${DOCKER_GWENWEB_NAME}_instance_${PID} -v `pwd`/gwen.properties:/opt/gwen-web/gwen.properties -u ${HOST_USER_ID}:${HOST_GROUP_ID} -v `pwd`:/tmp -v $FEATURE_DIRECTORY:/features -v $ABSOLUTE_REPORTS_PATH:/reports --link grid:hub gwen/gwenweb "-m /features/*.meta -p /opt/gwen-web/gwen.properties
 
   #sleep 10
 
