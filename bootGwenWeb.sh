018b6e2a6995660410904a3725213471499ba798
Ubuntu <ubuntu@ip-172-30-1-35.ec2.internal>
2015-11-08 07:02:13 +0000

    WIP

--- bootGwenWeb.sh
+++ bootGwenWeb.sh
@@ -33,12 +33,30 @@ function startSelenium () {
 }
 
 function startGwenWebSocket() {
-  docker run --name gwenwebws -u ${HOST_USER_ID}:${HOST_GROUP_ID} -v `pwd`:/tmp -v `pwd`/gwen.properties:/opt/gwen-web/gwen.properties -v /home/ibdev/repos/gwen-web/features/jkvine:/features -v `pwd`/reports:/reports --link grid:hub -p 8080:8080  --workdir='/opt/gwen-web' gwen/gwenwebws bash -c "/opt/websocketd --port=8080 --devconsole gwen-web-1.0.0-SNAPSHOT/bin/gwen-web -p /opt/gwen-web/gwen.properties -m /features/*.meta"
+  #docker run --name gwenwebws -u ${HOST_USER_ID}:${HOST_GROUP_ID} -v `pwd`:/tmp -v `pwd`/gwen.properties:/opt/gwen-web/gwen.properties -v /home/ibdev/repos/gwen-web/features/jkvine:/features -v `pwd`/reports:/reports --link grid:hub -p 8080:8080  --workdir='/opt/gwen-web' gwen/gwenwebws bash -c "/opt/websocketd --port=8080 --devconsole gwen-web-1.0.0-SNAPSHOT/bin/gwen-web -p /opt/gwen-web/gwen.properties -m /features/*.meta"
+  docker run --name gwenwebws -u ${HOST_USER_ID}:${HOST_GROUP_ID} -v `pwd`:/tmp -v `pwd`/gwen.properties:/opt/gwen-web/gwen.properties `pwd`/reports:/reports --link grid:hub -p 8080:8080  --workdir='/opt/gwen-web' gwen/gwenwebws bash -c "/opt/websocketd --port=8080 --devconsole gwen-web-1.0.0-SNAPSHOT/bin/gwen-web -p /opt/gwen-web/gwen.properties -m gwen-web-1.0.0-SNAPSHOT/features/jkvine/*.meta -r /reports"
 }
 
 function startGwen() {
  :
 }
 
-startSelenium
+
+if [[ $1 == 'startSelenium' ]]; then
+  startSelenium
+elif [[ $1 == 'stopSelenium' ]]; then
+  docker rm -f grid
+elif [[ $1 == 'startGwenWS' ]]; then
+  startGwenWebSocket
+elif [[ $1 == 'stopGwenWS' ]]; then
+  docker rm -f gwenwebws
+else
+ echo "To run bootGwenWeb <arg>"
+ echo  "  - startSelenium"
+ echo "  - stopSelenium"
+ echo "  - startGwenWS"
+ echo "  - stopGwenWS"
+fi
+
+#startSelenium
 #startGwenWebSocket
